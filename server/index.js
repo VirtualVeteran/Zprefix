1b1418a7852901e96b294458a445521d3f943a5f
VirtualVeteran <estheroswald001@yahoo.com>
2024-04-16 13:55:14 -0600

    creates app.get users successfully

--- server/index.js
+++ server/index.js
@@ -2,6 +2,31 @@ const express = require("express");
 
 const app = express();
 
-const port = 4000 ;
+const port = 8081 ;
+
+const knex = require('knex')(require('./knexfile.js')["development"]);
 
 app.listen(port, () => console.log('server is running on ${port}'));
+
+app.get('/', (request, response) => {
+    response.send('Application is working')
+})
+
+app.get('/user', async (request, response) => {
+    knex('user_account')
+    .select('*')
+    .then(data => {
+        var userNames = data.map(user => user.username); // Change user_account to data
+        response.json(userNames);
+    })
+    .catch(error => {
+        console.error('Error fetching user accounts:', error);
+        response.status(500).json({ error: 'Failed to fetch user accounts' });
+    });
+});
+// app.get('/launches', async (req, res) => {
+//     const allLaunches = await knex('launch').select('*');
+
+
+//     res.status(200).json('All the launches')
+// });
