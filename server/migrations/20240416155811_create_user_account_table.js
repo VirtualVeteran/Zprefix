195497cc4e583c92ba1eec8c1d1c37a47e04188e
VirtualVeteran <estheroswald001@yahoo.com>
2024-04-16 10:00:31 -0600

    creates migration folder

---
+++ server/migrations/20240416155811_create_user_account_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('user_account', table => {
+      table.increments('id');
+      table.string('username');
+      table.string('password')
+    })
+};
+
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('user_account');
+  };
