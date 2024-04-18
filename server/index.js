f89b799b628c4f660f1646fb1d7a020d8e673fb0
VirtualVeteran <estheroswald001@yahoo.com>
2024-04-18 16:41:37 -0600

    changes app.post endpoint to match salted passwords

--- server/index.js
+++ server/index.js
@@ -27,16 +27,23 @@ app.get('/user', async (req, res) => {
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    try {
-        const user = await knex('user_account').where({ username, password }).first();
-        if (user) {
-            res.status(200).json({ message: 'Login successful', user });
+    try{
+        const user = await knex('user_account').where({ username }).first();
+        
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+        
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (passwordMatch) {
+            return res.status(200).json({ message: 'Login successful', user });
         } else {
-            res.status(401).json({ message: 'Invalid username or password' });
+            return res.status(401).json({ message: 'Invalid username or password' });
         }
     } catch (error) {
         console.error('Error authenticating user:', error);
-        res.status(500).json({ message: 'Internal server error' });
+        return res.status(500).json({ message: 'Internal server error' });
     }
 });
 
